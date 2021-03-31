a4628aee3bfbcbddd50614bfb9cbde0c099bfd62
maddy362 <madhusudansharma2108@gmail.com>
2021-03-31 15:09:40 +0530

    UI updated

--- src/main/java/com/cg/tms/manualTesting/RouteUI.java
+++ src/main/java/com/cg/tms/manualTesting/RouteUI.java
@@ -23,30 +23,45 @@ public class RouteUI {
 	public void start() {
 		Route route1 = new Route();
 		Bus bus1 = new Bus();
+		Bus bus2 = new Bus();
 		Route route2 = new Route();
 		route1.setRouteId("R1");
 		route1.setRouteFrom("Jaipur");
 		route1.setRouteTo("Delhi");
+		
 		route1.setFare(600);
 		bus1.setBusType("AC");
 		bus1.setBusNumber("B1");
 		bus1.setCapacity(15);
 		bus1 = busService.addBus(bus1);
-		route1.setBuses(busService.viewBusList());
+		bus2.setBusType("AC/Seater");
+		bus2.setBusNumber("B2");
+		bus2.setCapacity(20);
+		bus2 = busService.addBus(bus2);
+		List<Bus> busList1 = new ArrayList<>();
+		busList1.add(bus1);
+		busList1.add(bus2);
+		route1.setBuses(busList1);
+
+		
 		route2.setRouteId("R2");
 		route2.setRouteFrom("Delhi");
 		route2.setRouteTo("Himachal");
 		route2.setFare(600);
+		
+
 		route1 = service.addRoute(route1);
 		route2 = service.addRoute(route2);
 		display(route1);
 		display(route2);
+
 		route1.setRouteId("R1");
 		route1.setRouteFrom("Jaipur");
 		route1.setRouteTo("Himachal");
 		route1.setFare(1200);
 		route1 = service.updateRoute(route1);
 		display(route1);
+
 		Route route3 = service.searchRoute("R2");
 		display(route3);
 		List<Route> list = new ArrayList<>();
@@ -61,9 +76,7 @@ public class RouteUI {
 		System.out.println("Route From = " + route.getRouteFrom());
 		System.out.println("Route To = " + route.getRouteTo());
 		System.out.println("Route Fare = " + route.getFare());
-		for (Bus bus : route.getBuses()) {
-			System.out.println(bus);
-		}
+
 	}
 
 }
