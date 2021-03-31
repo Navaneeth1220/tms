75e905109087b3d7e67348a67ff7d4c16583e996
MohanaSundaramPurushothaman <mp1429@srmist.edu.in>
2021-03-31 17:06:57 +0530

    Restcontroller added

--- src/main/java/com/cg/tms/controllers/CustomerRestController.java
+++ src/main/java/com/cg/tms/controllers/CustomerRestController.java
@@ -1,10 +1,24 @@
 package com.cg.tms.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.tms.dto.CreateCustomerRequest;
+import com.cg.tms.dto.CustomerDetails;
+import com.cg.tms.dto.DeleteCustomerRequest;
+import com.cg.tms.entities.Customer;
 import com.cg.tms.service.ICustomerService;
+import com.cg.tms.util.CustomerUtil;
 
 @RequestMapping("/customers")
 @RestController
@@ -12,5 +26,37 @@ public class CustomerRestController {
 
 
 	@Autowired
-	private ICustomerService Service;
+	private ICustomerService customerService;
+	@Autowired
+	private CustomerUtil customerUtil;
+
+	@GetMapping(value = "/byid/{id}")
+	public CustomerDetails fetchCustomer(@PathVariable("id") int CustomerId) {
+
+		Customer customer = customerService.viewCustomer(CustomerId);
+		CustomerDetails customerDetails = customerUtil.toDetailCustomer(customer);
+		return customerDetails;
+	}
+
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/addCustomer")
+	public String addCustomer(@RequestBody CreateCustomerRequest requestData) {
+
+		Customer customer = new Customer();
+		customer.setCustomerName(requestData.getCustomerName());
+		customer.setCustomerPassword(requestData.getCustomerPassword());
+		customer.setAddress(requestData.getAddress());
+		customer.setMobileNo(requestData.getMobileNo());
+		customer.setEmail(requestData.getEmail());
+		Customer added = customerService.addCustomer(customer);
+		return "added package with packageId =" + added.getCustomerId();
+	}
+
+	@DeleteMapping("/deleteCustomer")
+	public void deleteCustomer(@RequestBody DeleteCustomerRequest requestData) {
+
+		customerService.deleteCustomer(requestData.getCustomer());
+	}
+
 }
