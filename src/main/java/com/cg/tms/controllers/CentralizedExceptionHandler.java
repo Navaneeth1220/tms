ac6b9f46925871b2e954175fec0dc88f605f3a60
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-04-02 09:31:42 +0530

    exception handler

--- src/main/java/com/cg/tms/controllers/CentralizedExceptionHandler.java
+++ src/main/java/com/cg/tms/controllers/CentralizedExceptionHandler.java
@@ -48,6 +48,98 @@ public class CentralizedExceptionHandler {
 	}
 
 
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public String handleCustomerNotFound(CustomerNotFoundException e) {
+
+		return e.getMessage();
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidCustomerNameException.class)
+	public String handleInvalidCustomerName(InvalidCustomerNameException e) {
+
+		return e.getMessage();
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidCustomerAddressException.class)
+	public String handleInvalidCustomerAddress(InvalidCustomerAddressException e) {
+
+		return e.getMessage();
+	}
+
+ 	@ExceptionHandler(BookingNotFoundException.class)
+	public String handleBookingNotFound(BookingNotFoundException e) {
+
+		return e.getMessage();
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidBookingException.class)
+	public String handleInvalidBooking(InvalidBookingException e) {
+
+		return e.getMessage();
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(ReportNotFoundException.class)
+	public String handleReportNotFound(ReportNotFoundException e) {
+
+		return e.getMessage();
+	}
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidReportException.class)
+	public String handleInvalidReport(InvalidReportException e) {
+
+		return e.getMessage();
+	}
+
+                 @ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(RouteNotFoundException.class)
+	public String handleRouteNotFound(RouteNotFoundException e) {
+
+		return e.getMessage();
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidRouteException.class)
+	public String handleInvalidRoute(InvalidRouteException e) {
+
+		return e.getMessage();
+	}
+
+                 @ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidFeedbackException.class)
+	public String handleInvalidFeedback(InvalidFeedbackException e) {
+
+		return e.getMessage();
+	}
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(FeedbackNotFoundException.class)
+	public String handleFeedbackNotFound(FeedbackNotFoundException e) {
+
+		return e.getMessage();
+	}
+
+                 @ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(TravelsNotFoundException.class)
+	public String handleTravelsNotFound(TravelsNotFoundException e) {
+
+		return e.getMessage();
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(InvalidTravelException.class)
+	public String handleInvalidTravels(InvalidTravelException e) {
+
+		return e.getMessage();
+	}
+	
+               
+
+
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
 	public String handleCatchAllException(Exception e) {
